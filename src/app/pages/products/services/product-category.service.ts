88779d419118f90b3e8d2f1472b9521bc8613acb
chingaloEric <echingalo@gmail.com>
2019-11-12 16:59:53 +0300

    feat: added firebase querying methods to the application services
    Extended the base service inorder to implement firebase queries

--- src/app/pages/products/services/product-category.service.ts
+++ src/app/pages/products/services/product-category.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { BaseService } from 'src/app/shared/services/base.service';
-import { Product } from '../models/product.model';
+import { ProductCategory } from '../models/product-category.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductsService extends BaseService<Product> {
+export class ProductCategoryService extends BaseService<ProductCategory> {
   constructor(angularFirestore: AngularFirestore, afAuth: AngularFireAuth) {
     super(angularFirestore, afAuth);
   }
 
   get model() {
-    return 'products';
+    return 'product-categories';
   }
 }
